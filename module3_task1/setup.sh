cbf81f651741ff1da23da80b6184411903b2fb84
cbmarie255 <court.b1993@gmail.com>
2023-01-24 12:50:44 -0800

    changes to setup

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -4,3 +4,5 @@ apt-get install -y hugo make golang-go git curl
 curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_0.84.0_Linux-64bit.deb -o hugo.deb
 apt install ./hugo.deb
 apt-get install -y shellcheck
+go mod init github.com/cbmarie255/awesome-api
+go build
