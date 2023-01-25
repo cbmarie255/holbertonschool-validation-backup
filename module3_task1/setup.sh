f48ed11d0a0325c72a98433fe6bc7682561739d8
cbmarie255 <court.b1993@gmail.com>
2023-01-25 12:43:11 -0800

    trying again

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -4,3 +4,4 @@ apt-get install -y hugo make golang-go git curl
 curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_0.84.0_Linux-64bit.deb -o hugo.deb
 apt install ./hugo.deb
 apt-get install -y shellcheck
+make build
