5d4fbea0c71ee15c641424f8c16b67b4dbe0741e
cbmarie255 <court.b1993@gmail.com>
2023-01-26 10:04:32 -0800

    removing build process

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -11,5 +11,3 @@ rm -rvf /usr/local/go
 wget https://dl.google.com/go/go1.19.5.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.5.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
-make build
-
