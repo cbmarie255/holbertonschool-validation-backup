9c09e1fea828d2e7265345273cb888c4731c6ddb
cbmarie255 <court.b1993@gmail.com>
2023-01-26 07:41:56 -0800

    trying to fix setup file to install all needed packages

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-apt-get update -y
-apt-get install -y hugo make golang-go git curl
+sudo sudo apt-get update -y
+sudo apt-get install -y hugo make golang-go git curl
 curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_0.84.0_Linux-64bit.deb -o hugo.deb
-apt install ./hugo.deb
-apt-get install -y shellcheck
+sudo apt install ./hugo.deb
+sudo apt-get install -y shellcheck
+sudo apt-get install -y markdownlint
+sudo apt-get remove golang-go
+sudo apt-get remove --auto-remove golang-go
+sudo rm -rvf /usr/local/go
+wget https://dl.google.com/go/go1.14.linux-amd64.tar.gz
 make build
