7984eadda121cbc57dfcc4143f6db1371af61d69
cbmarie255 <court.b1993@gmail.com>
2023-01-26 11:39:00 -0800

    changing path for go setup

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -6,8 +6,8 @@ sudo apt install ./hugo.deb
 sudo apt-get install -y shellcheck
 sudo apt-get install -y markdownlint
 sudo apt-get remove -y golang-go
-apt-get remove -y --auto-remove golang-go
+sudo apt-get remove -y --auto-remove golang-go
 rm -rvf /usr/local/go
 wget https://dl.google.com/go/go1.19.5.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.5.linux-amd64.tar.gz
-export PATH=$PATH:/usr/local/go/bin
+export PATH=/usr/local/go/bin:$PATH
