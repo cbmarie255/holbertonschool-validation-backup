516520eec1f03fc0e528374a7c581a54b9b25f3f
cbmarie255 <court.b1993@gmail.com>
2023-01-26 12:10:56 -0800

    removing markdown lint package

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -3,8 +3,6 @@ sudo sudo apt-get update -y
 sudo apt-get install -y hugo make golang-go git curl
 curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_0.84.0_Linux-64bit.deb -o hugo.deb
 sudo apt install ./hugo.deb
-sudo apt-get install -y shellcheck
-sudo apt-get install -y markdownlint
 sudo apt-get remove -y golang-go
 sudo apt-get remove -y --auto-remove golang-go
 rm -rvf /usr/local/go
