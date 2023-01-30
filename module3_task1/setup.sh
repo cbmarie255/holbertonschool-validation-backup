8227ad419036a80167f7dcc2d4db95661ecf170e
cbmarie255 <97993217+cbmarie255@users.noreply.github.com>
2023-01-30 16:31:39 -0600

    Adding download for shellcheck

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-sudo sudo apt-get update -y
+sudo apt-get update -y
+wget -qO- https://github.com/koalaman/shellcheck/releases/download/v0.7.0/shellcheck-v0.7.0.linux.x86_64.tar.xz | tar -xJf -
+cd shellcheck-v0.7.0/
+sudo cp shellcheck /usr/local/bin
 sudo apt-get install -y hugo make golang-go git curl
 curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_0.84.0_Linux-64bit.deb -o hugo.deb
 sudo apt install ./hugo.deb
