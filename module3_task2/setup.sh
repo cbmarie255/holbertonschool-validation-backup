dc97aa9c3af5dbee4bd92ff55c51497d7cec3af1
cbmarie255 <court.b1993@gmail.com>
2023-02-03 11:12:26 -0800

    setup.sh adds

--- module3_task2/setup.sh
+++ module3_task2/setup.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 sudo apt-get update -y
 sudo apt-get install -y shellcheck
+sudo apt install -y zip
+sudo apt-get install -y npm
+npm install -g n
+n 16.0.0
+hash -r
+npm install -g npm@7.10.0
+npm install -g markdownlint-cli
 sudo apt-get install -y hugo make golang-go git curl
 curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_0.84.0_Linux-64bit.deb -o hugo.deb
 sudo apt install ./hugo.deb
