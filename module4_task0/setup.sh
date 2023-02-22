e64a7cec50a5e1ea77368631a446310f73aa36a5
cbmarie255 <court.b1993@gmail.com>
2023-02-22 14:40:05 -0600

    taking away golang install for checker

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -8,13 +8,6 @@ n 16.0.0
 hash -r
 npm install -g npm@7.10.0
 npm install -g markdownlint-cli
-sudo apt-get install -y hugo make golang-go git curl
+sudo apt-get install -y hugo make
 curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_0.84.0_Linux-64bit.deb -o hugo.deb
 sudo apt install ./hugo.deb
-sudo apt-get remove -y golang-go
-sudo apt-get remove -y --auto-remove golang-go
-rm -rvf /usr/local/go
-wget https://dl.google.com/go/go1.19.5.linux-amd64.tar.gz
-rm -rf ./go && tar -C . -xzf go1.19.5.linux-amd64.tar.gz
-currentDirectory=$(pwd)
-export PATH=$currentDirectory/go/bin:$PATH
