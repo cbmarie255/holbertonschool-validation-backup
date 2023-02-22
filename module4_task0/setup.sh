77cc5412fa3777cb7477d168da2b3289f32d7b75
cbmarie255 <court.b1993@gmail.com>
2023-02-22 15:08:06 -0600

    trying Brandon's setup script

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -6,8 +6,13 @@ sudo apt-get install -y npm
 npm install -g n
 n 16.0.0
 hash -r
-npm install -g npm@7.10.0
+npm install netlify-cli -g
+npm install -g npm@9.4.0
 npm install -g markdownlint-cli
+sudo apt-get install curl -y
 sudo apt-get install -y hugo make
-curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_0.84.0_Linux-64bit.deb -o hugo.deb
+curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
 sudo apt install ./hugo.deb
+sudo apt install zip
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.5.linux-amd64.tar.gz
+export PATH=/usr/local/go/bin:$PATH
