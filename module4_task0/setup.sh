ed4f0b80a83d7002daf60d82fa920ce629160e9c
cbmarie255 <court.b1993@gmail.com>
2023-02-23 10:55:27 -0600

    pushing new setup for checker

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
-sudo apt-get update -y
-sudo apt-get install -y shellcheck
-sudo apt install -y zip
-sudo apt-get install -y npm
+apt-get update && apt-get install curl make npm
 npm install -g n
 n 16.0.0
 hash -r
 npm install netlify-cli -g
-npm install -g npm@9.4.0
-npm install -g markdownlint-cli
-sudo apt-get install curl -y
-sudo apt-get install -y hugo make
+apt-get update && apt-get install -y hugo make
+apt-get install curl -y
 curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
-sudo apt install ./hugo.deb
+apt-get install ./install_hugo.deb
+npm install npm@latest -g -y
+npm install -g npm@9.4.0
+npm install -g markdownlint-cli -g
 sudo apt install zip
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.5.linux-amd64.tar.gz
 export PATH=/usr/local/go/bin:$PATH
