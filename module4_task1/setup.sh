2e6371c4a2788a5502883a356447580e0a30ef37
cbmarie255 <court.b1993@gmail.com>
2023-02-24 12:50:19 -0600

    editing the setup file

--- module4_task1/setup.sh
+++ module4_task1/setup.sh
@@ -4,10 +4,7 @@ npm install -g n
 n 16.0.0
 hash -r
 npm install netlify-cli -g
-apt-get update && apt-get install -y hugo make
 apt-get install curl -y
-curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
-apt-get install ./install_hugo.deb
 npm install npm@latest -g -y
 npm install -g npm@9.4.0
 npm install -g markdownlint-cli -g
